7b910402c34cd8d73bac28f0ff77a33aa9690a01
Amitoje Singh Bakshi <amitojeSBakshi@users.noreply.github.com>
2026-09-11 23:51:10 +0530

    Refresh: 55 new events, new startup and expo filters, expired cleanup

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿/* Pune Circle - build 20260909-194433-5662 */
-var CACHE = "pune-circle-20260909-194433-5662";
+﻿/* Pune Circle - build 20260911-181541-7331 */
+var CACHE = "pune-circle-20260911-181541-7331";
 var ASSETS = ["./", "./index.html", "./manifest.json",
               "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
